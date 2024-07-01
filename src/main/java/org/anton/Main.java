9be5fe9d1c1282e269e3c3be5ed846b9b20e27a9
Anton Lobanov <ailobanov97@mail.ru>
2024-07-01 18:02:33 +0300

    feat: Longest Substring
    Add my and hexlet solutions for finding longest substring

--- src/main/java/org/anton/Main.java
+++ src/main/java/org/anton/Main.java
@@ -1,9 +1,10 @@
 package org.anton;
 
-import static org.anton.hexlet.module2.lists.HM2Lists.HEXLET_calcInPolishNotation;
+import static org.anton.hexlet.module2.lists.HM2Lists.HEXLET_getLongestLength;
+import static org.anton.hexlet.module2.lists.HM2Lists.getLongestLength;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(HEXLET_calcInPolishNotation("1 2 + 4 * 3 +"));
+        System.out.println(HEXLET_getLongestLength("abcdeafghh"));
     }
 }
--- src/main/java/org/anton/hexlet/module2/lists/HM2Lists.java
+++ src/main/java/org/anton/hexlet/module2/lists/HM2Lists.java
@@ -1,18 +1,15 @@
 package org.anton.hexlet.module2.lists;
 
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
-//**************************************************************************************************************|
-//                                          Reverse Polish Notation, RPN                                        |
-//**************************************************************************************************************|
-//                                                                                                              |
-//                                                 My Solution                                                  |
 
 public class HM2Lists {
+    //**************************************************************************************************************|
+    //                                          Reverse Polish Notation, RPN                                        |
+    //______________________________________________________________________________________________________________|
+    //                                                 My Solution                                                  |
+    //                                                                                                              |
     public static int calcInPolishNotation(String expression) {
         int var1 = 0, var2 = 0, pos = 0, result = 0;
         ArrayList<String> signs = new ArrayList<>(Arrays.asList("+", "-", "*", "/"));
@@ -50,15 +47,16 @@ public class HM2Lists {
                 throw new ArithmeticException();
         }
     }
-//                                                                                                              |
-//                                             Hexlet Solution                                                  |
-//                                                                                                              |
+
+    //______________________________________________________________________________________________________________|
+    //                                             Hexlet Solution                                                  |
+    //                                                                                                              |
     public static int HEXLET_calcInPolishNotation(String expression) {
         String[] values = expression.split(" ");
         var stack = new LinkedList<Integer>();
         var operators = List.of("+", "-", "*", "/");
 
-        for (var value: values) {
+        for (var value : values) {
             if (!operators.contains(value)) {
                 stack.add(Integer.parseInt(value));
                 continue;
@@ -68,8 +66,122 @@ public class HM2Lists {
             var result = calculate(a, b, value);
             stack.add(result);
         }
-
         return stack.pollLast();
     }
+
+    //**************************************************************************************************************|
+    //                                              Longest Substring                                               |
+    //______________________________________________________________________________________________________________|
+    //                                                 My Solution                                                  |
+    public static int getLongestLength(String symbols) {
+        if (symbols.isEmpty()) return 0;
+        else if (symbols.length() == 1) return 1;
+        else {
+            for (int i = symbols.length(); i > 1; i--) {
+                if (checkSubstring(createSubstrings(symbols, i))) return i;
+            }
+        }
+        return 1;
+    }
+
+    private static String[] createSubstrings(String line, int subLength) {
+        String[] substrings = new String[line.length() - subLength + 1];
+        for (int i = 0; i < substrings.length; i++) {
+            substrings[i] = line.substring(i, i + subLength);
+        }
+        return substrings;
+    }
+
+    private static boolean checkSubstring(String[] substrings) {
+        for (String substring : substrings) {
+            char[] chars = substring.toCharArray();
+            HashSet<Character> set = new HashSet<>();
+            for (char c : chars) {
+                set.add(c);
+            }
+            if (substring.length() == set.size()) return true;
+        }
+        return false;
+    }
+
+    //______________________________________________________________________________________________________________|
+    //                                             Hexlet Solution                                                  |
+    //                                                                                                              |
+    public static int HEXLET_getLongestLength(String str) {
+        var sequence = new ArrayList<Character>();
+        var maxLength = 0;
+        for (var currentChar : str.toCharArray()) {
+            var index = sequence.indexOf(currentChar);
+            sequence.add(currentChar);
+            if (index != -1) {
+                sequence.subList(0, index + 1).clear();
+            }
+            int sequenceLength = sequence.size();
+            if (sequenceLength > maxLength) {
+                maxLength = sequenceLength;
+            }
+        }
+        return maxLength;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
