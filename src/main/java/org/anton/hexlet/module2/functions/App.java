3760608acff561e7bfe58c2ba777f9ee7fca2aef
Anton Lobanov <ailobanov97@mail.ru>
2024-07-07 17:37:25 +0300

    geat: Group words by lambda parameter

--- src/main/java/org/anton/hexlet/module2/functions/App.java
+++ src/main/java/org/anton/hexlet/module2/functions/App.java
@@ -1,8 +1,5 @@
 package org.anton.hexlet.module2.functions;
-;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
@@ -36,5 +33,36 @@ public class App {
             if (!pr.test(word)) return false;
         }
         return true;
+    }
+
+    //**************************************************************************************************************|
+    //                                           Group words by lambda parameter                                    |
+    //______________________________________________________________________________________________________________|
+    //                                                   My Solution                                                |
+    public static Map<String, List<String>> groupBy(List<String> words, Function<String, String> fn) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String word : words) {
+            String key = fn.apply(word);
+            map.putIfAbsent(key, new ArrayList<>());
+            List<String> temp = map.get(key);
+            temp.add(word);
+            map.put(key, temp);
+        }
+        return map;
+    }
+
+    //______________________________________________________________________________________________________________|
+    //                                              Hexlet Solution                                                 |
+    public static Map<String, List<String>> HEXLET_groupBy(List<String> coll, Function<String, String> fn) {
+        var result = new HashMap<String, List<String>>();
+        coll.forEach((item) -> {
+            var key = fn.apply(item);
+            result.compute(key, (k, wordsGroup) -> {
+                wordsGroup = wordsGroup == null ? new ArrayList<>() : wordsGroup;
+                wordsGroup.add(item);
+                return wordsGroup;
+            });
+        });
+        return result;
     }
 }
