268bfbc5040e5af410ac102d7206526ebbe06511
Anton Lobanov <ailobanov97@mail.ru>
2024-07-10 13:45:40 +0300

    feat: Get list of same parity numbers

--- src/main/java/org/anton/hexlet/module2/streams/HM2Streams.java
+++ src/main/java/org/anton/hexlet/module2/streams/HM2Streams.java
@@ -1,6 +1,7 @@
 package org.anton.hexlet.module2.streams;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
@@ -24,5 +25,29 @@ public class HM2Streams {
                 .filter(user -> user.get("gender").equals("male"))
                 .map(user -> LocalDate.parse(user.get("birthday")).getYear())
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+    }
+
+    //**************************************************************************************************************|
+    //                                        Get list of same parity numbers                                       |
+    //______________________________________________________________________________________________________________|
+    //                                                 My Solution                                                  |
+    public static List<Integer> sameParityFilter(List<Integer> numbers) {
+        if (numbers.isEmpty()) return numbers;
+        boolean even = numbers.getFirst() % 2 == 0;
+        return numbers.stream().filter(number -> (number % 2 == 0) == even).toList();
+    }
+
+    //______________________________________________________________________________________________________________|
+    //                                             Hexlet Solution                                                  |
+    public static List<Integer> HEXLET_sameParityFilter(List<Integer> coll) {
+        if (coll.isEmpty()) {
+            return new ArrayList<Integer>();
+        }
+        final boolean firsItemParity = isEven(coll.get(0));
+        return coll.stream().filter(item -> isEven(item) == firsItemParity).toList();
+    }
+
+    private static boolean isEven(int num) {
+        return num % 2 == 0;
     }
 }
